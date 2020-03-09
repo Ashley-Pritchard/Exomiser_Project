c2f1cb4fccab67c426ef8a299c8d2816ea1aa553
Ashley-Pritchard <45206729+Ashley-Pritchard@users.noreply.github.com>
2020-03-09 12:34:06 +0000

    Updated variable names for better clarity

--- run_exomiser/pull_vcf_patient_IDs.py
+++ run_exomiser/pull_vcf_patient_IDs.py
@@ -1,22 +1,23 @@
+#script to pull the 269 hypertrophic cardiomyopathy patient IDs from the CSD3 and save them as a text file for reference in later analysis
 import glob
 import re
 import sys
 import csv
 
 #select all vcf files
-vcf_paths = glob.glob('/home/hpcprit1/rds/rds-who1000-wgs10k/WGS10K/data/release/20180228-A/vcf/HCM/*.vcf.gz')
+vcf_file_path = glob.glob('/home/hpcprit1/rds/rds-who1000-wgs10k/WGS10K/data/release/20180228-A/vcf/HCM/*.vcf.gz')
 
 #make empty list for vcf names 
-vcf_files = []
+vcf_IDs = []
 
 #iterate through file names and split on '/' and '_' to pull out the vcf id - append to the empty list  
-for vcf in vcf_paths:
-        vcf_file = re.split('/|_', vcf)[11]
-        vcf_files.append(vcf_file)
+for vcf in vcf_file_path:
+        vcf_ID = re.split('/|_', vcf)[11]
+        vcf_IDs.append(vcf_ID)
 
 #write the list of vcf names to a text file with each name on a new line
-with open('vcfs.txt', 'w') as f:
+with open('vcf_IDs.txt', 'w') as f:
         writer = csv.writer(f, lineterminator='\n')
-        for vcf in vcf_files:
-                writer.writerow([vcf])
+        for vcf_ID in vcf_IDs:
+                writer.writerow([vcf_ID])
 
