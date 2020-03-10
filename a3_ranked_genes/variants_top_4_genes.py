c295ede1dc62103f2232ee1b3a79e7791b7d12dc
Ashley-Pritchard <45206729+Ashley-Pritchard@users.noreply.github.com>
2020-03-10 12:13:59 +0000

    Updated comments for clarity

--- a3_ranked_genes/variants_top_4_genes.py
+++ a3_ranked_genes/variants_top_4_genes.py
@@ -1,3 +1,5 @@
+#script to pull HGVS name and respective patient IDs for all variants occuring in the top 4 ranked genes (DES, NAGLU, MYH7 and MYOZ2). Data output as csv files. 
+
 import argparse
 import glob
 import os
@@ -5,18 +7,20 @@ import re
 import pandas as pd
 import sys
 
-#request command line input of the cutoff value for the combined score to be applied
+#request command line input of the run name for analysis 
 parser = argparse.ArgumentParser()
 parser.add_argument("run_name", help='input the name of the run you are analysing')
 args = parser.parse_args()
 
-#pull all variant analysis files output from exomiser 
+#output files from each Exomiser run were transferred from the CSD3 server to a 'exomiser_output_files' directory on the local machine
+#pull all variant analysis files for specified run
 exomiser_files = glob.glob('/home/ashley/Documents/Exomiser_Project/exomiser_transfer/exomiser_output_files/' + args.run_name + '/*.variants.tsv')
 
+#set empty lists for collecting patient and variant data
 patient = []
 variant = []
 
-#iterate through files and append combined score to empty list 
+#iterate through files and append respective HGVS and patient ID data for variants occuring in genes of interest
 for ex in exomiser_files:
 	df = pd.read_csv(ex, sep='\t')
 	for index, row in df.iterrows():
@@ -33,9 +37,11 @@ for ex in exomiser_files:
 			variant.append(row[9])
 			patient.append(re.split('/', ex)[7])
 
+#make dictionary of pulled patient and variant data 
 dict = { 'patient': patient, 'variant' : variant }
 
-#make a new dataframe of variant counts
+#make a new dataframe using the dictionary created
 df = pd.DataFrame(dict)
 
-df.to_csv('/home/ashley/Documents/Exomiser_Project/exomiser_transfer/analysis/' + args.run_name + '/_top_4' + '.csv', sep=',', header = True)
+#write data to csv file
+df.to_csv(args.run_name + '_variants_in_top_genes' + '.csv', sep=',', header = True)
