0a4d2db77c60a60c08a5d3525989ccd2cd11e502
Ashley-Pritchard <45206729+Ashley-Pritchard@users.noreply.github.com>
2020-03-11 15:44:48 +0000

    Add files via upload

---
+++ random_case_generator.py
@@ -0,0 +1,21 @@
+#script to generate a random sample of the 30 / 269 HCM patients for variant interpretation
+
+import argparse
+import glob
+import random 
+
+#pull all gene analysis files output from exomiser (named as patient IDs)
+exomiser_file_path = glob.glob('/home/ashley/Documents/Exomiser_Project/exomiser_transfer/exomiser_output_files/' + args.run_name + '/*.html')
+
+#create an empty list to store patient IDs
+patient_IDs = []
+
+#split the file paths to pull the individual patient IDs from the names and store in created list
+for ex in exomiser_file_path:
+	exomiser_files.append(ex.split('/')[8])
+
+#create the random sample of 30 patient IDs
+sampling = random.choices(exomiser_files, k=30)
+
+#print the sample to the terminal 
+print(sampling)
